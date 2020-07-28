c5e38b9e6feaaea34e334a314224f21ff5681685
Zachary Belford <belfordz66@gmail.com>
2020-07-27 21:45:51 -0700

    feat: generate code for patternProperties

---
+++ src/integration-tests/expecteds/py/patternProperties.py
@@ -0,0 +1,13 @@
+from typing import NewType
+
+BooleanVyG3AETh = NewType("BooleanVyG3AETh", bool)
+from typing import NewType
+
+Godlike = NewType("Godlike", float)
+from typing import NewType
+
+AbsoluteMadLad = NewType("AbsoluteMadLad", str)
+from typing import TypedDict, Optional
+
+class Milkers(TypedDict):
+    anything: Optional[BooleanVyG3AETh]
