d7b23a43525d8ee8e4942b79a8877e53f0804ca5
Shane Jonas <jonas.shane@gmail.com>
2023-01-17 17:43:40 -0500

    fix: languageSafeName leading digits to words
    also added comments for regexes

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,5 +1,5 @@
 import { JSONSchemaObject } from "@json-schema-tools/meta-schema";
-import {replaceTypeAsArrayWithOneOf} from "./utils";
+import {replaceTypeAsArrayWithOneOf, languageSafeName} from "./utils";
 
 describe('utils', () => {
   it('can handle no titles on typeArrays', () => {
@@ -9,3 +9,26 @@ describe('utils', () => {
     expect(results.title?.includes("Undefined")).toEqual(false);
   });
 })
+
+describe("languageSafeName", () => {
+  it("should return a capitalized and deburred string with safe characters", () => {
+    const testCases = [
+      { title: "32 hex encoded bytes", expected: "ThreeTwoHexEncodedBytes" },
+      { title: "64 hex encoded bytes", expected: "SixFourHexEncodedBytes" },
+      { title: "hex encoded bytes 32", expected: "HexEncodedBytes32" },
+      { title: "hello world!", expected: "HelloWorld" },
+      { title: "Hello World!", expected: "HelloWorld" },
+      { title: "Hello_World!", expected: "HelloWorld" },
+      { title: "Hello_World1!", expected: "HelloWorld1" },
+      { title: "Hello_World1toot!", expected: "HelloWorld1Toot" },
+      { title: "Hello_World1Toot!", expected: "HelloWorld1Toot" },
+      { title: "Hello_World1_Test!", expected: "HelloWorld1Test" },
+      { title: "Hello World 1!", expected: "HelloWorld1" },
+    ];
+
+    testCases.forEach(({ title, expected }) => {
+      const result = languageSafeName(title);
+      expect(result).toEqual(expected);
+    });
+  });
+});
